144849a87a805aabb125763c9b27cc025474eea9
lemonfunny <liuguiyang6559@gmail.com>
2024-07-02 07:44:20 +0000

    Delete Post Draft source/_drafts/在群晖上部署nastool全过程 by桜紺狸.md by Qexo UTC 2024-07-02 Tuesday 07:44:20 Updated By Github Actions

---
+++ anzhiyu/random.js
@@ -0,0 +1,56 @@
+var posts=["archives/5607dff5/","archives/25263640/","archives/ebe9814e/","archives/f66e1c0f/","archives/166b9040/","archives/26e8b5d3/","archives/edcb4e27/","archives/7126f402/","archives/b4d66110/","archives/5ec9035e/","archives/1d8d801b/","archives/d2bef6c/","archives/8186ed29/","archives/3dbd4859/","archives/1c9aa8da/","archives/500551f5/","archives/e1e728f5/","archives/8024b874/","archives/601b2243/","archives/1d51d142/","archives/32e7f725/","archives/e877a0cf/","archives/d8199f01/","archives/276dafca/","archives/a57f6ad8/"];function toRandomPost(){
+    pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
+  };var friend_link_list=[{"name":"豹纸小站","link":"https://www.baopaper.cn","avatar":"https://baopaper.cn/img/favicon.webp","descr":"互联网浪潮中的一所小木屋。"},{"name":"玄名の博客","link":"https://xuan-ming.github.io/","avatar":"https://img.imgdb.cn/item/5f2d44a214195aa594f45549.png","descr":"来自好友の友联"},{"name":"小理の小窝","link":"https://xiaolii.com","avatar":"https://source.xiaolii.com/Imagesource/icon.jpg","descr":"一个普通学生的普通生活"},{"name":"Eacls","link":"https://www.eacls.top/","avatar":"https://www.eacls.top/img/favicon.png","descr":"看一看世界的繁华"},{"name":"幻梦のblogs","link":"https://fantasy.kim/","avatar":"https://ik.imagekit.io/skira/skirablog/friend/3d19084809bae71d2747b.jpg?updatedAt=1690718544193","descr":null},{"name":"YzMzxl's Blog","link":"https://gitblog.hutaowife.com/","avatar":"https://imgoss.top/file/5a5187d7aad8875e36785.jpg","descr":"发现，探索，分享，总结"},{"name":"wyj5211的小站","link":"https://blog.wyj5211.top/","avatar":"https://gravatar.loli.net/avatar/5802ef407e84292358999b15c6cd39de?s=500","descr":"折腾学习无止境"},{"name":"NMSL","link":"https://nmsl.cn/","avatar":"https://nmsl.cn/icon.png","descr":"永远不要理会谣言和中伤"},{"name":"棕果核","link":"https://mrzgh.top/","avatar":"https://mrzgh.top/img/avatar.png","descr":"生命不息，折腾不止"},{"name":"醉浪の博客","link":"https://blog.zuilang.tk","avatar":"https://blog.zuilang.tk/img/favicon.png","descr":"一只喜欢七彩球的枯叶蝶"},{"name":"LyShark - 孤风洗剑","link":"https://www.lyshark.com","avatar":"https://ik.imagekit.io/skira/skirablog/friend/2c399da9a146b8606f89d.webp?updatedAt=1690718470680","descr":"道，可道，非常道；名，可名，非常名。—— 老子《道德经》"},{"name":"Hercules's Blog","link":"https://blog.zhaoziyi.site","avatar":"https://im.lemonbuluo.eu.org/file/50b73cdac8bf64dba5b85.jpg","descr":"数理化生一塌糊涂的理科生，对兴趣情有独钟的追寻！"},{"name":"MoyuqLのBlog","link":"https://blog.moyuql.top/","avatar":"https://bu.dusays.com/2023/04/23/64447bfbc1b1c.webp","descr":"MoyuqL与你同在~"},{"name":"Thun888’s Blog","link":"https://blog.hzchu.top","avatar":"https://blog.hzchu.top/img/avatar.webp","descr":"夏日当空,心如深渊"},{"name":"葱苓","link":"https://blog.itciraos.cn","avatar":"https://cdn.jsdelivr.net/gh/ciraos/ciraos-static@main/img/avatar1.webp","descr":"Dare && Do"},{"name":"屑殇","link":"https://jinghuashang.cn/","avatar":"https://jinghuashang.cn/img/acc.webp","descr":"桃李春风一杯酒，江湖夜雨十年灯"},{"name":"SuYi-宿仪","link":"https://www.thotz.top/","avatar":"https://pic.imgdb.cn/item/64c09ca51ddac507cc7f1585.png","descr":"随心写作，随缘阅读"},{"name":"九夏小站","link":"https://blog.inekoxia.com","avatar":"https://blog.inekoxia.com/imgs/logo.png","descr":"日常摸鱼咕咕咕"},{"name":"Qeem","link":"http://itstarqeem.space","avatar":"https://pic.imgdb.cn/item/64c65629a9eb352efd93a379.webp","descr":"阿巴阿巴…"},{"name":"Dahi Blog","link":"https://dahi.icu/","avatar":"https://s1.ax1x.com/2023/08/01/pP9zpXd.png","descr":"愿你有一天能與你重要的人重逢"},{"name":"安知鱼","link":"https://blog.anheyu.com/","avatar":"https://npm.elemecdn.com/anzhiyu-blog-static@1.0.4/img/avatar.jpg","descr":"生活明朗，万物可爱"},{"name":"海底涝里没有鱼","link":"https://smileszh.cn","avatar":"https://bu.dusays.com/2023/06/10/648499e1299d0.jpg","descr":"平安喜乐，万事胜意"},{"name":"青桔气球","link":"https://blog.qjqq.cn/","avatar":"https://avatar.qjqq.cn/1/646cdb372913c.webp!avatar","descr":"分享网络安全与科技生活"},{"name":"小植同学","link":"https://blog.xiaoztx.top","avatar":"https://blog.xiaoztx.top/img/xiaozhi.jpg","descr":"生生不息，好运不止"},{"name":"CC康纳百川","link":"https://blog.ccknbc.cc","avatar":"https://cdn.chuqis.com/gh/ccknbc-backup/cdn/logo/ccknbc.png","descr":"CC康纳百川的小窝"},{"name":"铭心石刻","link":"https://blog.kouseki.cn/","avatar":"https://blog.kouseki.cn/imgs/avatar.webp","descr":"愿岁并谢，与友长兮"},{"name":"Lian’s Blog","link":"https://blog.kisskiss.store/","avatar":"https://blog.kisskiss.store/upload/logo.png","descr":"很潇洒却不能对你停止想念"},{"name":"Fan Blog","link":"https://blog.fanwu.link/","avatar":"https://tucdn.wpon.cn/2023/08/08/5b7218be570aa.png","descr":"Life goes on"},{"name":"Marcus","link":"https://blog.marcus233.top","avatar":"https://img01.anheyu.com/useruploads/8/2023/08/09/64d387fadfd13.png","descr":"人间值得，未来可期"},{"name":"吮指原味姬","link":"https://www.yejinblok.cn/","avatar":"https://img01.anheyu.com/useruploads/243/2023/08/15/64db8a22b3b83.ico","descr":"无论什么时候，都不要后悔"},{"name":"Knorth小柯北","link":"https://littleknorth.xyz/","avatar":"https://pic.imgdb.cn/item/64d62d151ddac507cc5cfd56.jpg","descr":"普通但不迂腐"},{"name":"未知之旅","link":"https://blog.xenosp.cn/","avatar":"https://cravatar.cn/avatar/4EA126708E5063621186D6B5895D2684","descr":"在未知中成长，超越自我"},{"name":"Chenli","link":"https://chenliss.cn/","avatar":"https://bu.dusays.com/2023/08/27/64eb384e49c82.jpg","descr":"记录生活,分享生活"},{"name":"寒烟志","link":"https://yanyuplus.cn/","avatar":"https://www.qiaoxiao.top/static/avater/avater.jpg","descr":"半山腰总是挤的，你得去山顶看看"},{"name":"Plasmon222","link":"https://blog.loveak.top/","avatar":"https://bu.dusays.com/2023/09/01/64f19e6f783a9.png","descr":"✨行则将至，做则必成✨"},{"name":"云深不知处","link":"https://www.lanzlz.cn/","avatar":"https://oss.lanzlz.cn/private/hIwTot.png","descr":"仙风道骨 清幽避世"},{"name":"纸鹿摸鱼处","link":"https://blog.zhilu.cyou","avatar":"https://cravatar.cn/avatar/13aa912754e6bb5e671f3e6654e4712d?s=640","descr":"纸鹿至麓不知路，支炉制露不止漉"},{"name":"Calyee","link":"https://blog.calyee.top/","avatar":"https://blog.calyee.top/img/avatar.jpg","descr":"追求充实，分享快乐"},{"name":"Rain’s Blog","link":"https://blog.im.ci/","avatar":"https://blog.im.ci/wp-content/uploads/2020/07/IMG_20191231_052259-scaled-e1596127234331.jpg","descr":"Rain的随笔小记"},{"name":"咖啡豆子coffee的小站","link":"https://kfdzcoffee.cn","avatar":"https://kfdzcoffee.cn/upload/mmexport1715001096100.png","descr":"所有奇迹的始发点"},{"name":"Fly","link":"http://flyme.cf/","avatar":"https://ik.imagekit.io/skira/skirablog/friend/fb3cfd4cf47ec13904c57.jpg?updatedAt=1690718199513","descr":"相逢的人会再相逢"},{"name":"Qinai’s Blog","link":"https://slqinai.cn","avatar":"http://q1.qlogo.cn/g?b=qq&nk=2140137093&s=640","descr":"夜の星もいつかは消える"}];
+    var refreshNum = 1;
+    function friendChainRandomTransmission() {
+      const randomIndex = Math.floor(Math.random() * friend_link_list.length);
+      const { name, link } = friend_link_list.splice(randomIndex, 1)[0];
+      Snackbar.show({
+        text:
+          "点击前往按钮进入随机一个友链，不保证跳转网站的安全性和可用性。本次随机到的是本站友链：「" + name + "」",
+        duration: 8000,
+        pos: "top-center",
+        actionText: "前往",
+        onActionClick: function (element) {
+          element.style.opacity = 0;
+          window.open(link, "_blank");
+        },
+      });
+    }
+    function addFriendLinksInFooter() {
+      var footerRandomFriendsBtn = document.getElementById("footer-random-friends-btn");
+      if(!footerRandomFriendsBtn) return;
+      footerRandomFriendsBtn.style.opacity = "0.2";
+      footerRandomFriendsBtn.style.transitionDuration = "0.3s";
+      footerRandomFriendsBtn.style.transform = "rotate(" + 360 * refreshNum++ + "deg)";
+      const finalLinkList = [];
+  
+      let count = 0;
+
+      while (friend_link_list.length && count < 3) {
+        const randomIndex = Math.floor(Math.random() * friend_link_list.length);
+        const { name, link, avatar } = friend_link_list.splice(randomIndex, 1)[0];
+  
+        finalLinkList.push({
+          name,
+          link,
+          avatar,
+        });
+        count++;
+      }
+  
+      let html = finalLinkList
+        .map(({ name, link }) => {
+          const returnInfo = "<a class='footer-item' href='" + link + "' target='_blank' rel='noopener nofollow'>" + name + "</a>"
+          return returnInfo;
+        })
+        .join("");
+  
+      html += "<a class='footer-item' href='/link/'>更多</a>";
+
+      document.getElementById("friend-links-in-footer").innerHTML = html;
+
+      setTimeout(()=>{
+        footerRandomFriendsBtn.style.opacity = "1";
+      }, 300)
+    };
